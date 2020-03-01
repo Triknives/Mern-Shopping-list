69e1b0e41cad9a8922e147854b53f5e800cccb7d
Triknives <50305423+Triknives@users.noreply.github.com>
2020-03-01 15:33:00 -0800

    sucessfully add backend and front end and allow CRUD functionality

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,7 +11,6 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import uuidv1 from 'uuid/v1';
 
 
 class ItemModal extends Component {
@@ -31,7 +30,6 @@ class ItemModal extends Component {
     e.preventDefault();
 
     const newItem = {
-      id: uuidv1(),
       name: this.state.name
     }
     // Add item via AddItem actions
