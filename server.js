5380db418ea4e9ca5a1cbb334fc8ed8658f8e6f4
Triknives <50305423+Triknives@users.noreply.github.com>
2020-03-10 20:53:58 -0700

    heroku prepare

--- server.js
+++ server.js
@@ -40,6 +40,16 @@ mongoose
   app.use ('/api/users', require('./routes/api/users'));
   app.use ('/api/auth', require('./routes/api/auth'));
 
-  const port = process.envPORT || 5000;
+  // Serve static asses if in production
+  if(process.env.node_env === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+  const port = process.env.PORT || 5000;
 
   app.listen(port, () => console.log(`Server started on port ${port}`));
