7e8922a7030cd4bc76d3df6158b2af61148eff82
Triknives <50305423+Triknives@users.noreply.github.com>
2020-03-11 13:06:54 -0700

    v5

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 //DB Config
 const db = config.get('mongoURI');
 
-
-
 //Connect to Mongo
 mongoose
   .connect(db, {
@@ -43,15 +41,14 @@ mongoose
   app.use ('/api/auth', require('./routes/api/auth'));
 
   // Serve static asses if in production
-  if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
+  if(process.env.node_env === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
 
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
+  }
 
   const port = process.env.PORT || 5000;
 
